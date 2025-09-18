4f65781e5d069f937c2c5f052e460c0968ec4bab
Davidson William <davidson.ferreira@qive.com.br>
2025-09-18 13:00:01 -0300

    fix: corrige o  regex

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const getPatterns = async () => {
 const applyListeners = (patterns) => {
   for (let words of patterns) {
     const { termo, explicacao, sugestoes } = words;
-    let regexPattern = new RegExp(`\\b${termo}\\b`);
+    let regexPattern = new RegExp(`\\b${termo}\\b`, "gi");
     const hasSuggestion = sugestoes
       ? {
           type: "context",
